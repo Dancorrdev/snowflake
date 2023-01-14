8784f408effc29eb1176860e4bc6a8732d1109b7
Danilo Cortes <dacortes63@misena.edu.co>
2023-01-14 00:53:06 -0500

    chore: apply single method responsibility

--- index.js
+++ index.js
@@ -1,47 +1,16 @@
 const express = require("express");
-const app = express();
-const faker = require("faker");
+const routesApi = require("./src/routes/index.routes");
 
+const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.get("/products", (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
-  res.json(products);
-});
-
-app.get("/products/:id", (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id,
-    name: "Pijama",
-    price: 65000,
-  });
-});
-
-app.get("/users", (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset,
-    });
-  } else {
-    res.send("No Hay Parametros");
-  }
-});
+routesApi(app);
 
 app.listen(port, () => {
   console.log("mi puerto", port);
