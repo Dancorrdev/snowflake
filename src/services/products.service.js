fcff0ca7f207d9e43cfdeeabd0c7649fde4d9767
Danilo Cortes <dacortes63@misena.edu.co>
2023-01-16 23:47:35 -0500

    chore: add async await to do data persistence for futures connections

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -18,25 +18,47 @@ class ProductsService {
     }
   }
 
-  create() {
-
+  async create(data) {
+    const newProduct = {
+      id: faker.datatype.uuid(),
+      ...data,
+    };
+    this.products.push(newProduct);
+    return newProduct;
   }
 
-  update(){
-
+  async update(id, changes) {
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw new Error("Product not found");
+    }
+    const product = this.products[index];
+    this.products[index] = {
+      ...product,
+      ...changes,
+    };
+    return this.products[index];
   }
 
-  find(){
-    return this.products;
+  find() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.products);
+      }, 5000);
+    });
   }
-  findById(id){
-    return this.products.find(item => item.id === id)
-
-  }
-  delete(){
-
+  async findById(id) {
+    return this.products.find((item) => item.id === id);
   }
 
+  async delete(id) {
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw new Error("Product not found");
+    }
+    this.products.splice(index, 1);
+    return { id };
+  }
 }
 
 module.exports = ProductsService;
