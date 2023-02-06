91d80c0d07480d56591e52fad9a266d0acd8e2bf
Danilo Cortes <dacortes63@misena.edu.co>
2023-02-06 15:56:10 -0500

    chore: add categories routing

---
+++ src/routes/categories.routes.js
@@ -0,0 +1,58 @@
+const { request } = require("express");
+const express = require("express");
+const CategoryService = require("./../services/category.service");
+
+const router = express.Router();
+const service = new CategoryService();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const cateries = await service.find();
+    res.json(cateries);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const category = await service.findById(id);
+    res.json(category);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const body = req.body;
+    const newCategory = await service.create(body);
+    res.status(201).json(newCategory);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.patch("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const category = await service.update(id, body);
+    res.json(category);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await service.delete(id);
+    res.status(201).json({ id });
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
